395ed22b0c630f20c306bf11d401963e2e36bf02
trungdangtd <trungdang1301@gmail.com>
2024-10-20 01:01:21 +0700

    UX/UI for login and register screens

---
+++ lib/views/screens/authentication_screens/register_screen.dart
@@ -0,0 +1,298 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:multi_store_app/views/screens/authentication_screens/login_screen.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(
+        0.95,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Đăng ký",
+                    style: GoogleFonts.getFont('Lato',
+                        color: const Color(0xFF0d120E),
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.2,
+                        fontSize: 25),
+                  ),
+                  Text(
+                    "Chào mừng bạn quay trở lại",
+                    style: GoogleFonts.getFont('Lato',
+                        color: const Color(0xFF0d120E),
+                        letterSpacing: 0.2,
+                        fontSize: 16),
+                  ),
+                  Image.asset(
+                    'assets/images/Illustration.png',
+                    width: 300,
+                    height: 300,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Email',
+                      style: GoogleFonts.getFont('Nunito Sans',
+                          fontWeight: FontWeight.w600, letterSpacing: 0.2),
+                    ),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Email không được để trống';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: 'Nhập email của bạn',
+                      labelStyle: GoogleFonts.getFont('Nunito Sans',
+                          fontSize: 14, letterSpacing: 0.1),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/email.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Tên đăng nhập',
+                      style: GoogleFonts.getFont('Nunito Sans',
+                          fontWeight: FontWeight.w600, letterSpacing: 0.2),
+                    ),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Tên đăng nhập không được để trống';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: 'Nhập tên của bạn',
+                      labelStyle: GoogleFonts.getFont('Nunito Sans',
+                          fontSize: 14, letterSpacing: 0.1),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/user.jpeg',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Mật khẩu',
+                      style: GoogleFonts.getFont('Nunito Sans',
+                          fontWeight: FontWeight.w600, letterSpacing: 0.2),
+                    ),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Mật khẩu không được để trống';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: 'Nhập mật khẩu của bạn',
+                      labelStyle: GoogleFonts.getFont('Nunito Sans',
+                          fontSize: 14, letterSpacing: 0.1),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/password.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                      suffixIcon: const Icon(Icons.visibility),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        print('correct');
+                      } else {
+                        print('incorrect');
+                      }
+                    },
+                    child: Container(
+                      width: 320,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color(0xFF102DE1),
+                            Color(0xcc0d6eff),
+                          ],
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 278,
+                            top: 19,
+                            child: Opacity(
+                              opacity: 0.5,
+                              child: Container(
+                                width: 60,
+                                height: 60,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 12,
+                                    color: const Color(0xFF103DE5),
+                                  ),
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 260,
+                            left: 29,
+                            child: Opacity(
+                              opacity: 0.5,
+                              child: Container(
+                                width: 10,
+                                height: 10,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 3),
+                                    color: const Color(0xFF2141E5),
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 311,
+                            top: 36,
+                            child: Opacity(
+                              opacity: 0.3,
+                              child: Container(
+                                width: 5,
+                                height: 5,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(3)),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 281,
+                            top: -10,
+                            child: Opacity(
+                              opacity: 0.3,
+                              child: Container(
+                                width: 20,
+                                height: 20,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              'Đăng ký',
+                              style: GoogleFonts.getFont('Lato',
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Bạn đã có tài khoản?',
+                        style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w500, letterSpacing: 1),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return LoginScreen();
+                          }));
+                        },
+                        child: Text(
+                          'Đăng nhập',
+                          style: GoogleFonts.roboto(
+                              fontWeight: FontWeight.w500,
+                              color: const Color(0xFF103DE5)),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
